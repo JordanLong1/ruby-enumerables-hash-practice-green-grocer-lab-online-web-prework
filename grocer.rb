e1a1594325091c02146271fdba5e0e83fc28f0f0
Jordan Long <jordanleelong5@gmail.com>
2019-10-01 18:15:50 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -18,30 +18,20 @@ end
 final_hash
 end
 
-def apply_coupons(cart:[], coupons:[])
-  # code here	  coupons.each do |coupon|
-    coupon_name = coupon[:item]
-    coupon_item_num = coupon[:num]
-    cart_item = cart[coupon_name]
-
-    next if cart_item.nil? || cart_item[:count] < coupon_item_num
-
-    cart_item[:count] -= coupon_item_num
-
-    coupon_in_cart = cart["#{coupon_name} W/COUPON"]
-
-    if coupon_in_cart
-      coupon_in_cart[:count] += 1
-    else
-      cart["#{coupon_name} W/COUPON"] = {
-        price: coupon[:cost],
-        clearance: cart_item[:clearance],
-        count: 1
-      }
-    end
-  end
-
-  return cart
+def apply_coupons(cart, coupons)
+  coupons.each do |coupon|
+    item = coupon[:item]
+    if cart.has_key?(item)
+      if cart[item][:count] >= coupon[:num] && !cart.has_key?("#{item} W/COUPON")
+        cart["#{item} W/COUPON"] = {price: coupon[:cost] / coupon[:num] , clearance: cart[item][:clearance], count: coupon[:num]}
+        cart[item][:count] -= coupon[:num]
+    elsif cart[item][:count] >= coupon[:num] && cart.has_key?("#{item} W/COUPON")
+        cart["#{item} W/COUPON"][:count] += coupon[:num]
+        cart[item][:count] -= coupon[:num]
+      end
+        end
+          return cart
+      end
 end
 
 
